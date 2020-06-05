4511c74938a885527e810f9c9c6c6738a6d3ef94
katthartic <katt.baum@gmail.com>
2020-06-04 21:25:36 -0400

    Fix mapview

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import React from 'react'
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import MapView from 'react-native-maps'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView />
+      <MapView
+        style={styles.mapStyle}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
       <Text>Open up App.js to start working on your app!</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-});
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+})
