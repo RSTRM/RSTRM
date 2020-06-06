3e68664923a149febfdbad1e8aa3aaf58633e5ea
Denis McPhillips <denismcphillips@gmail.com>
2020-06-06 17:58:17 -0400

    added seed data, half mile radius, and user's location

--- components/GoogleMapView.js
+++ components/GoogleMapView.js
@@ -1,25 +1,64 @@
-import React from 'react'
-import { StyleSheet, Dimensions } from 'react-native'
-import MapView from 'react-native-maps'
+import React, { useState, useEffect } from "react";
+import { Text, StyleSheet, Dimensions } from "react-native";
+import MapView, { Marker, Circle } from "react-native-maps";
+import seedArray from "../assets/initialSeed";
+import * as Location from "expo-location";
 
 export default GoogleMapView = () => {
+  const [region, setRegion] = useState({
+    latitude: 40.7061,
+    longitude: -73.9969,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  });
+
+  const onRegionChangeComplete = (region) => {
+    setRegion(region);
+  };
+
+  if (!location || !region) {
+    return <Text>Loading...</Text>;
+  }
   return (
     <MapView
       style={styles.mapStyle}
       provider="google"
-      initialRegion={{
-        latitude: 40.7061,
-        longitude: -73.9969,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
-  )
-}
+      initialRegion={region}
+      showsUserLocation={true}
+      onRegionChangeComplete={onRegionChangeComplete}
+    >
+      {seedArray.map((marker) => (
+        <Marker
+          coordinate={{
+            latitude: marker.latitude,
+            longitude: marker.longitude,
+          }}
+          title={marker.name}
+          description={marker.directions}
+        />
+      ))}
+      <Circle center={region} radius={500} />
+    </MapView>
+  );
+};
 
 const styles = StyleSheet.create({
   mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height - 196,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height - 196,
   },
-})
+});
