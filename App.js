752b79d11897debd3b756dec3cae17b9a3e3a8d0
rcntype@gmail.com <rcntype@gmail.com>
2020-06-08 19:48:19 -0600

    changed text on homescreen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons'
 import Home from './screens/Home'
 import Map from './screens/Map'
 
@@ -9,7 +10,22 @@ export default function App() {
   const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          let iconName;
+          if(route.name === 'Home'){
+            iconName = 'ios-home'
+          }
+          else if(route.name === 'Map'){
+            iconName = 'ios-globe'
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        }
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}>
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Map" component={Map} />
       </Tab.Navigator>
--- screens/Home.js
+++ screens/Home.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View, Button } from 'react-native'
 export default function Home({navigation}) {
     return (
         <View style={styles.container}>
-            <Text>Welcome to RSTRM! For when you gotta go...</Text>
+            <Text>Welcome to RSTRM!</Text>
+            <Text>For when you gotta go...</Text>
             <Button title="Go to Map"
                 onPress={() => navigation.navigate('Map')}
             />
