11cf91b9072bdb1c17dd43a78d5cac22f4ff0929
Valmik Vyas <valmik.vyas1@gmail.com>
2020-06-11 19:50:02 -0400

    UI Restroom

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './screens/Home'
 import Map from './screens/Map'
+import RestroomView from './screens/Restroom'
 
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
       <Tab.Navigator>
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Map" component={Map} />
+        <Tab.Screen name="Restroom" component={RestroomView} />
       </Tab.Navigator>
     </NavigationContainer>
   )
