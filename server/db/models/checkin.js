0dfcae668c60eca55986f05ab847bb531796ef8d
katthartic <katt.baum@gmail.com>
2020-06-17 06:20:59 -0400

    Add checkin and review for every bathroom

--- server/db/models/checkin.js
+++ server/db/models/checkin.js
@@ -29,9 +29,9 @@ module.exports = Checkin
 
 Checkin.afterCreate(async function (checkin) {
   const user = await db.models.user.findByPk(checkin.userId)
-  console.log(user.id, ' - ', user.totalCheckins)
+  // console.log(user.id, ' - ', user.totalCheckins)
   const totalCheckins = user.totalCheckins + 1
-  console.log(totalCheckins)
-  const update = await user.update({ totalCheckins })
-  console.log(update)
+  // console.log(totalCheckins)
+  const updatedUser = await user.update({ totalCheckins })
+  // console.log(updatedUser._changed)
 })
