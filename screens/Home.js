474cfd72a20cce12995fb670a12ca45cf133e0f3
katthartic <katt.baum@gmail.com>
2020-06-19 21:36:19 -0400

    Add user profile screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,28 +1,29 @@
 import React from 'react'
 import { StyleSheet, Text, View, Button } from 'react-native'
 
-
-export default function Home({navigation}) {
-    return (
-        <View style={styles.container}>
-            <Text>Welcome to RSTRM!</Text>
-            <Text>For when you gotta go...</Text>
-            <Button title="User Sign/Login"
-                onPress={() => navigation.navigate('User')}
-            />
-            <Button title="Account Settings"
-                onPress={() => navigation.navigate('Setting')}
-            />
-        </View>
-    );
+export default function Home({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to RSTRM!</Text>
+      <Text>For when you gotta go...</Text>
+      <Button
+        title="User Sign/Login"
+        onPress={() => navigation.navigate('User')}
+      />
+      <Button
+        title="User Profile"
+        onPress={() => navigation.navigate('UserProfile')}
+      />
+      <Button title="Settings" onPress={() => navigation.navigate('Setting')} />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 })
-
--- screens/HomeStack.js
+++ screens/HomeStack.js
@@ -1,17 +1,18 @@
-import * as React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
 import Home from './Home'
 import User from './User'
 import Setting from './Setting'
-
+import UserProfile from './UserProfile'
 
 export default function HomeStack() {
-    const HomeStack = createStackNavigator();
-    return (
-        <HomeStack.Navigator>
-            <HomeStack.Screen name="Home" component={Home} />
-            <HomeStack.Screen name="User" component={User} />
-            <HomeStack.Screen name="Setting" component={Setting} />
-        </HomeStack.Navigator>
-    );
+  const HomeStack = createStackNavigator()
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={Home} />
+      <HomeStack.Screen name="User" component={User} />
+      <HomeStack.Screen name="User Profile" component={UserProfile} />
+      <HomeStack.Screen name="Setting" component={Setting} />
+    </HomeStack.Navigator>
+  )
 }
