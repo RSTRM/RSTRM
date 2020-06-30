49f3ad5366d6d6104de41306163c4efc70826ee5
katthartic <katt.baum@gmail.com>
2020-06-30 06:04:28 -0400

    Add redux logger

--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
-// import { createLogger } from "redux-logger";
+import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { bathrooms } from "./bathrooms";
@@ -18,8 +18,13 @@ const reducer = combineReducers({
   userItems
 });
 
-const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware));
+// const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware));
 
-const store = createStore(reducer, middleware);
+// const store = createStore(reducer, middleware);
+
+const middleware = composeWithDevTools(
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+)
+const store = createStore(reducer, middleware)
 
 export default store;
