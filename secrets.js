d81b2465af86a61747f7da6cfe9cdd409eb49acb
Valmik Vyas <valmik.vyas1@gmail.com>
2020-07-01 19:06:49 -0400

    secrets file changes

--- secrets.js
+++ secrets.js
@@ -1,9 +1,5 @@
 const GOOGLE_API_KEY = "AIzaSyAg3aikbYJ2TdSIyOE8ExZOmWx3xFeRHvU";
 const GOOGLE_CLIENT_ID =
   "764079452238-dv40q8v82dk0g6j74jn4n3ldggld74os.apps.googleusercontent.com";
-const AWS = {
-  accessKey: "AKIA2S5LYQMO62E43DVQ",
-  secretKey: "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"
-};
 
-export { GOOGLE_API_KEY, GOOGLE_CLIENT_ID, AWS };
+export { GOOGLE_API_KEY, GOOGLE_CLIENT_ID };
