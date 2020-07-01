9aeb6e8db6c690799d30a3e2250035d698d6f5e3
rcntype@gmail.com <rcntype@gmail.com>
2020-07-01 17:48:42 -0600

    google api erased

--- secret3.js
+++
@@ -1,5 +0,0 @@
-const GOOGLE_API_KEY = "AIzaSyAg3aikbYJ2TdSIyOE8ExZOmWx3xFeRHvU";
-const GOOGLE_CLIENT_ID =
-  "764079452238-dv40q8v82dk0g6j74jn4n3ldggld74os.apps.googleusercontent.com";
-
-export { GOOGLE_API_KEY, GOOGLE_CLIENT_ID };
